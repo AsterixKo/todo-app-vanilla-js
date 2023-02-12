78761e876d46dc50fc031624523e8ba4116e172b
Javier Moneo <jvr.moneo@gmail.com>
2023-02-12 12:03:47 +0100

    LocalStorage

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,10 +20,23 @@ const state = {
 const initStore = () => {
   console.log(state);
   console.log('InitStore');
+  loadStore();
 };
 
 const loadStore = () => {
-  throw new Error('Not implemented');
+  if (localStorage.getItem('state')) {
+    console.log('hay algo en local storage');
+    const { todos = [], filter = Filters.All } = JSON.parse(
+      localStorage.getItem('state')
+    );
+    state.todos = todos;
+    state.filter = filter;
+  }
+};
+
+const saveStateToLocalStorage = () => {
+  // console.log(JSON.stringify(state));
+  localStorage.setItem('state', JSON.stringify(state));
 };
 
 const getTodos = (filter = Filters.All) => {
@@ -47,12 +60,14 @@ const addTodo = (description) => {
   if (!description) throw new Error('Description is required');
 
   state.todos.push(new Todo(description));
+  saveStateToLocalStorage();
 };
 
 const toggleTodo = (todoId) => {
   state.todos = state.todos.map((todo) => {
     if (todo.id === todoId) {
       todo.done = !todo.done;
+      saveStateToLocalStorage();
     }
     return todo;
   });
@@ -60,14 +75,17 @@ const toggleTodo = (todoId) => {
 
 const deleteTodo = (todoId) => {
   state.todos = state.todos.filter((todo) => todo.id !== todoId);
+  saveStateToLocalStorage();
 };
 
 const deleteCompleted = () => {
   state.todos = state.todos.filter((todo) => todo.done);
+  saveStateToLocalStorage();
 };
 
 const setFilter = (newFilter = Filters.All) => {
   stateFilter = newFilter;
+  saveStateToLocalStorage();
 };
 
 const getCurrentFilter = () => {
@@ -83,5 +101,6 @@ export default {
   initStore,
   loadStore,
   setFilter,
+  saveStateToLocalStorage,
   toggleTodo,
 };
