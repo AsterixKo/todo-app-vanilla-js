bb7f7f436947920ff969ea6b559bf2c2eafee059
Javier Moneo <jvr.moneo@gmail.com>
2023-02-12 10:54:13 +0100

    Validaciones y optimizaciones

--- src/todos/use-cases/render-todos.js
+++ src/todos/use-cases/render-todos.js
@@ -1,6 +1,8 @@
 import { Todo } from '../models/todo.model';
 import { createTodoHTML } from './create-todo-html';
 
+let element;
+
 /**
  *
  * @param {String} elementId
@@ -8,8 +10,18 @@ import { createTodoHTML } from './create-todo-html';
  */
 export const renderTodos = (elementId, todos = []) => {
   // console.log(elementId, todos);
-  const element = document.querySelector(elementId);
-  todos.forEach(todo => {
+
+  if (!element) {
+    element = document.querySelector(elementId);
+  }
+
+  if (!element) {
+    throw new Error(`Element ${elementId} not found`);
+  }
+
+  element.innerHTML = '';
+
+  todos.forEach((todo) => {
     element.append(createTodoHTML(todo));
   });
 };
